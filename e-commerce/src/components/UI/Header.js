d6f90ebbff2e88e8d33497a76c2a8993eb81438e
Ben Fawley <benfawley0@gmail.com>
2023-01-07 23:21:37 +0000

    Fixed Header Bug

--- e-commerce/src/components/UI/Header.js
+++ e-commerce/src/components/UI/Header.js
@@ -4,26 +4,28 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>
-        <h1>Logo</h1>
+      <div className={classes.headerContent}>
+        <div className={classes.logo}>
+          <h1>Logo</h1>
+        </div>
+        <nav className={classes.navWrapper}>
+          <ul className={classes.categoryList}>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/mens">Mens</NavLink>
+            </li>
+            <li>
+              <NavLink to="/womens">Womens</NavLink>
+            </li>
+            <li>
+              <NavLink to="/activewear">Activewear</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={classes.icons}>Placeholder</div>
       </div>
-      <nav className={classes.navWrapper}>
-        <ul className={classes.categoryList}>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/mens">Mens</NavLink>
-          </li>
-          <li>
-            <NavLink to="/womens">Womens</NavLink>
-          </li>
-          <li>
-            <NavLink to="/activewear">Activewear</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className={classes.icons}>Placeholder</div>
     </header>
   );
 };
